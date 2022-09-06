700af838be86027695abec8dce6a298806cf4616
Hua Wang <eq9684@hotmail.com>
2022-09-06 09:12:51 +0800

    Update App.java

--- App.java
+++ App.java
@@ -17,31 +17,31 @@ public class App
          //     "accesskey",
          //     "securekey");
      
-        Region region = Region.US_WEST_2;
-        ConnectClient connect = ConnectClient.builder()
-            .region(region)
-            .credentialsProvider(credentialsProvider)
-            .build();
+         Region region = Region.US_WEST_2;
+         ConnectClient connect = ConnectClient.builder()
+             .region(region)
+             .credentialsProvider(credentialsProvider)
+             .build();
         
-        UserPhoneConfig phoneconfig = UserPhoneConfig.builder()
-        		.phoneType("SOFT_PHONE")
-        		.build();
+         UserPhoneConfig phoneconfig = UserPhoneConfig.builder()
+        	 	  .phoneType("SOFT_PHONE")
+        		   .build();
         
-        UserIdentityInfo useridentity = UserIdentityInfo.builder()
-        		.email("awssdk@midea.com")
-        		.firstName("aws")
-        		.lastName("sdk")
-        		.build();
+         UserIdentityInfo useridentity = UserIdentityInfo.builder()
+        		   .email("awssdk@midea.com")
+        		   .firstName("aws")
+        		   .lastName("sdk")
+        		   .build();
         
-        CreateUserRequest request = CreateUserRequest.builder()
-        		.instanceId("0fdb88c7-20c6-4a85-8a1e-068eecde0750")
-        		.username("awssdk")
-        		.password("Passw0rd")
-        		.phoneConfig(phoneconfig)
-        		.identityInfo(useridentity)
-        		.routingProfileId("62845221-ede4-4085-9f7c-a815be060185")
-        		.securityProfileIds("0ea37551-9437-460d-aab6-d4331686586a")
-        		.build();
+         CreateUserRequest request = CreateUserRequest.builder()
+        		   .instanceId("0fdb88c7-20c6-4a85-8a1e-068eecde0750")
+        		   .username("awssdk")
+        		   .password("Passw0rd")
+        		   .phoneConfig(phoneconfig)
+        		   .identityInfo(useridentity)
+        		   .routingProfileId("62845221-ede4-4085-9f7c-a815be060185")
+        		   .securityProfileIds("0ea37551-9437-460d-aab6-d4331686586a")
+        		   .build();
         
         connect.createUser(request);
     }
